2f2ec37dd30f4944966129128694ea031cfe5807
nomadbard916 <neonnova916@gmail.com>
2020-09-15 22:53:38 +0800

    frontend setup with vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,16 @@
+import Vue from 'vue';
+import router from './router';
+// @ts-ignore
+import App from './components/App';
+
 require('./bootstrap');
+
+// window.Vue = require('vue');
+
+const app = new Vue({
+  el: '#app',
+  components: {
+    App
+  },
+  router
+});
---
+++ resources/js/views/Start.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>qqqqqqq</div>
+</template>
+
+<script>
+export default {
+    name: "Start"
+};
+</script>
+
+<style></style>
