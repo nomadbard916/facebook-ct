bbe9805101fd9d41efa66a20697c9a5ac8c118d1
nomadbard916 <neonnova916@gmail.com>
2020-09-16 10:19:59 +0800

    setup tailwind

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,5 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');
 
 /*
  |--------------------------------------------------------------------------
@@ -18,4 +19,8 @@ mix.browserSync({
 });
 
 mix.js('resources/js/app.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css');
+  .sass('resources/sass/app.scss', 'public/css')
+  .options({
+    procesCssUrls: false,
+    postCss: [tailwindcss('./tailwind.config.js')]
+  });
