7c331f256cf98427b29c3333f60fb8f63d8480ed
nomadbard916 <neonnova916@gmail.com>
2020-09-16 15:35:32 +0800

    use provided assets to skip design section

--- tests/Feature/RetrievePostsTest.php
+++ tests/Feature/RetrievePostsTest.php
@@ -20,7 +20,7 @@ class RetrievePostsTest extends TestCase
             'api'
         );
 
-        $posts = factory(Post::class, 2)->create();
+        $posts = factory(Post::class, 2)->create(['user_id' => $user->id]);
 
         $response = $this->get('/api/posts');
 
@@ -31,18 +31,18 @@ class RetrievePostsTest extends TestCase
                     [
                         'data' => [
                             'type' => 'posts',
-                            'post_id' => $posts->first()->id,
+                            'post_id' => $posts->last()->id,
                             'attributes' => [
-                                'body' => $posts->first()->body
+                                'body' => $posts->last()->body
                             ]
                         ]
                     ],
                     [
                         'data' => [
                             'type' => 'posts',
-                            'post_id' => $posts->last()->id,
+                            'post_id' => $posts->first()->id,
                             'attributes' => [
-                                'body' => $posts->last()->body
+                                'body' => $posts->first()->body
                             ]
                         ]
                     ],
@@ -52,6 +52,25 @@ class RetrievePostsTest extends TestCase
                 'links' => [
                     'self' => url('/posts')
                 ]
+            ]);
+    }
+
+    /** @test */
+    public function a_user_can_only_retrieve_their_posts()
+    {
+        $this->actingAs(
+            $user = factory(User::class)->create(),
+            'api'
+        );
+
+        $posts = factory(Post::class)->create();
+
+        $response = $this->get('/api/posts');
+
+        $response->assertStatus(200)
+            ->assertExactJson([
+                'data' => [],
+                'links' => ['self' => url('/posts')]
             ]);
     }
 }
